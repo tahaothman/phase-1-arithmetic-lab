dccbfc1cbb7451154e19b500be443ebc91036c2c
Taha <taha.othman@outlook.com>
2023-03-17 21:44:41 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random()*10)+1;
+
+
+let num3 = 1070;
+let num4 = 13;
+let mod = num3 % num4;
+
+let max = Math.max(10,11,15,18,20);
